823aeae1c76917fb00c7c0a905baea3ff50916a4
shamanbronya <gavrylychevalex@gmail.com>
2018-11-21 13:14:28 +0200

    new

---
+++ memoryGame.js
@@ -0,0 +1,35 @@
+fetch('http://kde.link/test/get_field_size.php')
+  .then(response => {
+    return response.json();
+  }).then(data => getData(data))
+  .catch(() => console.log('Can\'t get response'));
+
+let picturesLinks = [
+  'https://kde.link/test/0.png',
+  'https://kde.link/test/1.png',
+  'https://kde.link/test/2.png',
+  'https://kde.link/test/3.png',
+  'https://kde.link/test/4.png',
+  'https://kde.link/test/5.png',
+  'https://kde.link/test/6.png',
+  'https://kde.link/test/7.png',
+  'https://kde.link/test/8.png',
+  'https://kde.link/test/9.png'
+];
+
+const table = document.getElementById('gameTable');
+
+function createCard (data) {
+  table.style.width = data.width * 120 + 'px';
+  table.style.height = data.height * 120 + 'px';
+  for(let i = 0; i < data.width; i++){
+    for(let j = 0; j < data.height; j++){
+      let card = document.createElement('div');
+      let img = document.createElement('img');
+      card.className = 'card';
+      img.src = picturesLinks[j];
+      card.appendChild(img);
+      table.appendChild(card);
+    }
+  }
+}
